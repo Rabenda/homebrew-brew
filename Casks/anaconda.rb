ae9fe4a16529a9bd35c903b83954e05331d149d9
Rabenda <rabenda.cn@gmail.com>
2019-04-12 00:44:55 +0800

    Add anaconda

---
+++ Casks/anaconda.rb
@@ -0,0 +1,38 @@
+cask 'anaconda' do
+  version '2019.03'
+  sha256 'b232f0b16181f48667d2ca89c04a4ee4b3932475282b41c52acb87b4cdafcaaf'
+
+  url "https://repo.anaconda.com/archive/Anaconda3-#{version}-MacOSX-x86_64.sh"
+  name 'Continuum Analytics Anaconda'
+  homepage 'https://www.anaconda.com/'
+
+  auto_updates true
+  container type: :naked
+
+  installer script: {
+                      executable: "Anaconda3-#{version}-MacOSX-x86_64.sh",
+                      args:       ['-b', '-p', "#{HOMEBREW_PREFIX}/anaconda3"],
+                      sudo:       true,
+                    }
+
+  postflight do
+    set_ownership "#{HOMEBREW_PREFIX}/anaconda3"
+  end
+
+  uninstall delete: [
+                      "#{HOMEBREW_PREFIX}/anaconda3",
+                      '/Applications/Anaconda-Navigator.app',
+                    ]
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.continuum.io.sfl*',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.continuum.python.sfl*',
+               '~/.anaconda',
+               '~/.conda',
+               '~/.condarc',
+             ]
+
+  caveats do
+    files_in_usr_local
+  end
+end
